845b2a5e63b8c97ad9eb25f2022bef27781c0e69
Marc Abramowitz <abramowi@adobe.com>
2018-02-14 10:33:07 -0800

    Omit data that is not computed
    so it's obvious that we are not computing these.
    
    Better of course to compute them, but let's do this in the meantime so
    we don't report a bunch of 0's that are not accurate.

--- cobertura.go
+++ cobertura.go
@@ -6,15 +6,15 @@ import (
 
 type Coverage struct {
 	XMLName         xml.Name   `xml:"coverage"`
-	LineRate        float32    `xml:"line-rate,attr"`
-	BranchRate      float32    `xml:"branch-rate,attr"`
+	LineRate        float32    `xml:"line-rate,attr,omitempty"`
+	BranchRate      float32    `xml:"branch-rate,attr,omitempty"`
 	Version         string     `xml:"version,attr"`
 	Timestamp       int64      `xml:"timestamp,attr"`
-	LinesCovered    int64      `xml:"lines-covered,attr"`
-	LinesValid      int64      `xml:"lines-valid,attr"`
-	BranchesCovered int64      `xml:"branches-covered,attr"`
-	BranchesValid   int64      `xml:"branches-valid,attr"`
-	Complexity      float32    `xml:"complexity,attr"`
+	LinesCovered    int64      `xml:"lines-covered,attr,omitempty"`
+	LinesValid      int64      `xml:"lines-valid,attr,omitempty"`
+	BranchesCovered int64      `xml:"branches-covered,attr,omitempty"`
+	BranchesValid   int64      `xml:"branches-valid,attr,omitempty"`
+	Complexity      float32    `xml:"complexity,attr,omitempty"`
 	Sources         []*Source  `xml:"sources>source"`
 	Packages        []*Package `xml:"packages>package"`
 }
@@ -25,18 +25,18 @@ type Source struct {
 
 type Package struct {
 	Name       string   `xml:"name,attr"`
-	LineRate   float32  `xml:"line-rate,attr"`
-	BranchRate float32  `xml:"branch-rate,attr"`
-	Complexity float32  `xml:"complexity,attr"`
+	LineRate   float32  `xml:"line-rate,attr,omitempty"`
+	BranchRate float32  `xml:"branch-rate,attr,omitempty"`
+	Complexity float32  `xml:"complexity,attr,omitempty"`
 	Classes    []*Class `xml:"classes>class"`
 }
 
 type Class struct {
 	Name       string    `xml:"name,attr"`
 	Filename   string    `xml:"filename,attr"`
-	LineRate   float32   `xml:"line-rate,attr"`
-	BranchRate float32   `xml:"branch-rate,attr"`
-	Complexity float32   `xml:"complexity,attr"`
+	LineRate   float32   `xml:"line-rate,attr,omitempty"`
+	BranchRate float32   `xml:"branch-rate,attr,omitempty"`
+	Complexity float32   `xml:"complexity,attr,omitempty"`
 	Methods    []*Method `xml:"methods>method"`
 	Lines      []*Line   `xml:"lines>line"`
 }
@@ -44,9 +44,9 @@ type Class struct {
 type Method struct {
 	Name       string  `xml:"name,attr"`
 	Signature  string  `xml:"signature,attr"`
-	LineRate   float32 `xml:"line-rate,attr"`
-	BranchRate float32 `xml:"branch-rate,attr"`
-	Complexity float32 `xml:"complexity,attr"`
+	LineRate   float32 `xml:"line-rate,attr,omitempty"`
+	BranchRate float32 `xml:"branch-rate,attr,omitempty"`
+	Complexity float32 `xml:"complexity,attr,omitempty"`
 	Lines      []*Line `xml:"lines>line"`
 }
 
